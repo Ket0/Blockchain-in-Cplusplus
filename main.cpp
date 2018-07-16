ad33a2d46d4a986136aa1f1ba30982e1cd27bad7
Ket0 <36997534+Ket0@users.noreply.github.com>
2018-07-16 18:09:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,13 +15,6 @@ int main(){
 
     cout << "Blockchain in C++" << endl;
 
-    Block myBlock;
-    Block myBlock2;
-
-    //
-    myBlock.Setm_data("Hallo vom ersten Block.");
-    myBlock2.Setm_data("Hallo vom zweiten Block.");
-
     // Leeren Container anlegen
     std::cout << "Erstelle Blockchain."
     std::vector<Block> myBlockchain;
@@ -29,16 +22,11 @@ int main(){
     //
     std::cout << "Erstelle Genesisblock."
     meinGenesisBlock = erzeugeGenesisblock();
-
-    // Block einfügen
-    myBlockchain.push_back(myBlock);
-    myBlockchain.push_back(myBlock2);
-
+    
     // Ausgeben
     for (auto x : myBlockchain){
         std::cout << "Block Payload: " << x.Getm_data() << "\n";
     }
-
-
+    
     return 0;
 }
