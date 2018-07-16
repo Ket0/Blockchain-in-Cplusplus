2a7b6f6339d9fefae243d37074aca4cc530ea525
Ket0 <36997534+Ket0@users.noreply.github.com>
2018-07-16 18:13:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,32 @@
+// 
+// 
+// // Einfache Blockchain in C++11 \\
+//
+// Version: 0.1
+//
+// Autor : Stefan Eickholz
+// Datum:   16.07.2018
+// Ort:     Potsdam
+// Sprache: C++
+// Version: 11
+//
+// In den folgend gezeigten Programmanweisungen wird meine Python Blockchain
+// in C++ umgesetzt.
+// 
+// 
 
-
+// Includes
 #include <iostream>
 #include <vector>
-
 #include "Block.h"
-
 using namespace std;
 
+// Funktionen
 Block erzeugeGenesisBlock(){
     // erzeuge
 };
 
 int main(){
-
     cout << "Blockchain in C++" << endl;
 
     // Leeren Container anlegen
